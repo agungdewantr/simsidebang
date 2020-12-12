1b0a4c594897977d3cfa86fb00337fada540b32d
agungdewantr <69377282+agungdewantr@users.noreply.github.com>
2020-12-13 00:26:08 +0700

    fix sprint5

--- app/Http/Controllers/keuanganController.php
+++ app/Http/Controllers/keuanganController.php
@@ -39,7 +39,11 @@ class keuanganController extends Controller
                                 'omzet' => $omzet->omzet,
                                 'keuntungan' => $keuntungan]
                             );
+                            $keuangan = keuangan::paginate(10);
+                            return view('CRUDkeuangan-read', compact('keuangan'));
                           }
+                          $keuangan = keuangan::paginate(10);
+                          return view('CRUDkeuangan-read', compact('keuangan'));
       }
       $keuangan = keuangan::paginate(10);
       return view('CRUDkeuangan-read', compact('keuangan'));
@@ -59,69 +63,4 @@ class keuanganController extends Controller
                   }
         }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\keuangan  $keuangan
-     * @return \Illuminate\Http\Response
-     */
-    public function show(keuangan $keuangan)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\keuangan  $keuangan
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(keuangan $keuangan)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\keuangan  $keuangan
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, keuangan $keuangan)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\keuangan  $keuangan
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(keuangan $keuangan)
-    {
-        //
-    }
 }
